8119a9bb7ff7ec86349e1d8b0be0322baf413748
KieranBFH <115044082+KieranBFH@users.noreply.github.com>
2023-03-15 14:06:48 -0600

    tells player what cpu played + wording changes

--- main.py
+++ main.py
@@ -1,40 +1,45 @@
 import random
-ai = ["r", "p", "c"]
+ai = ["Rock", "Paper", "Scissors"]
 p1 = " "
 p2 = " "
+
+
 def whowon():
     global p1
+    p1.capitalize()
     global p2
+    p2.capitalize()
     if p1 == p2:
         return(0)
-    elif p1 == "r":
-        if p2 == "p":
+    elif p1 == "Rock":
+        if p2 == "Paper":
             return(2)
-        elif p2 == "s":
+        elif p2 == "Scissors":
             return(1)
         else:
             return(-1)
-    elif p1 == "p":
-        if p2 == "r":
+    elif p1 == "Paper":
+        if p2 == "Rock":
             return(1)
-        elif p2 == "s":
+        elif p2 == "Scissors":
             return(2)
         else:
             return(-1)
-    elif p1 == "s":
-        if p2 == "r":
+    elif p1 == "Scissors":
+        if p2 == "Rock":
             return(2)
-        elif p2 =="p":
+        elif p2 =="Paper":
             return(1)
         else:
             return(-1)
     else:
         return(-1)
 
-mode = input("Select mode my g (m/s): ")
+
+mode = input("Multiplayer or Singleplayer? (m/s): ")
 if mode == 'm':
-    p1 = input("Player 1 Rock Paper or Scissors (r, p, s) ")
-    p2 = input("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nPlayer 2 Rock Paper or Scissors (r, p s) ")
+    p1 = input("Player 1: Rock Paper or Scissors? ")
+    p2 = input("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nPlayer 2: Rock Paper or Scissors?  ")
     result = whowon()
     if result == -1:
         print('error')
@@ -46,14 +51,14 @@ if mode == 'm':
         print("Player 2 Won")
 
 else:
-    p1 = input("Player 1 Rock Paper or Scissors (r, p, s) ")
+    p1 = input("Player 1: Rock Paper or Scissors?")
     p2 = random.choice(ai)
     result = whowon()
     if result == -1:
-        print('error')
+        print('Error. Did you spell the options correctly?')
     elif result == 0:
-        print("Tie")
+        print("Tie! CPU played " + p2)
     elif result == 1:
-        print("You won!")
+        print("You won! CPU played" + p2)
     elif result == 2:
-        print("You lost.")
+        print("You lost. CPU played " + p2)
